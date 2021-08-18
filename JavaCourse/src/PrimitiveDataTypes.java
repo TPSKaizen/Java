dcbc3b9979a4229b6091ca7bcfba58065743b507
Brian McConney <briancamcc@gmail.com>
2021-08-18 18:38:59 -0400

    "Added code for primitive data types"

--- JavaCourse/src/PrimitiveDataTypes.java
+++ JavaCourse/src/PrimitiveDataTypes.java
@@ -4,7 +4,28 @@ public class PrimitiveDataTypes {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-
+		char a = 'A';
+		
+		byte b = 2;
+		
+		short c = 22;
+		
+		int d = 45;
+		
+		float e = 5.123451f;
+		
+		double f = 4.68295233535d;
+		
+		boolean g = true;
+		
+		System.out.println("Char " + a);
+		System.out.println("byte " + b);
+		System.out.println("short " + c);
+		System.out.println("int " + d);
+		System.out.println("float " + e);
+		System.out.println("double " + f);
+		System.out.println("bool " + g);
+		
 	}
 
 }
