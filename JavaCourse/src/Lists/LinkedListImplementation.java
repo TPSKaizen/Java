d48b65e89f100bf52c1d66561697a5964196e301
Brian McConney <briancamcc@gmail.com>
2021-08-23 15:37:49 -0400

    "Added LinkedList implementation"

--- JavaCourse/src/Lists/LinkedListImplementation.java
+++ JavaCourse/src/Lists/LinkedListImplementation.java
@@ -1,12 +1,36 @@
 package Lists;
 
 import java.util.List;
+import java.util.LinkedList;
 
 public class LinkedListImplementation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
+		
+		LinkedList<String> list = new LinkedList<String>();
+		
+		list.add("A");
+		list.add("B");
+		list.add("B");
+		//Add C at the end
+		list.addLast("C");
+		//Add D at the beginning
+		list.addFirst("D");
+		//insert E at index 2
+		list.add(2, "E");
+		
+		//remove the first instance of element with value B
+		list.remove("B");
+		
+		//remove element at index 3
+		list.remove(3);
+		
+		list.removeFirst();
+		
+		list.removeLast();
+		
+		System.out.println(list);
 	}
 
 }
---
+++ JavaCourse/src/Sets/TreeSet.java
@@ -0,0 +1,10 @@
+package Sets;
+
+public class TreeSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
