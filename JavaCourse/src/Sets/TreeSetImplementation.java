3a0940229ade667fc4f7b03352b19dc9444cd18a
Brian McConney <briancamcc@gmail.com>
2021-08-23 16:18:59 -0400

    "Added treeSet implementation"

--- JavaCourse/src/Sets/TreeSetImplementation.java
+++ JavaCourse/src/Sets/TreeSetImplementation.java
@@ -1,9 +1,34 @@
 package Sets;
 
-public class TreeSetImplementation {
+import java.util.TreeSet;
 
+public class TreeSetImplementation {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		/*
+		 * When we have to maintain the natural order of
+		 * the elements automatically, we use treeSet
+		 * Cannot have duplicates, not synchronized, not thread-safe
+		 */
+		
+		TreeSet<String> tSet = new TreeSet<String>();
+		//String here tells Java how to sort the tree set
+		/* However, if you use a custom class, you have to tell java
+		 * how to sort the elements
+		 * It has similar methods to hashset
+		 */
+
+		
+		tSet.add("B");
+		tSet.add("A");
+		tSet.add("C");
+		tSet.add("C");
+		
+		System.out.println(tSet);
+		// Will print A B C, the natural order is preserved
+		
 
 	}
 
