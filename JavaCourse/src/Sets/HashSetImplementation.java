8d5c39cd42bf5f9955b838e7f762663dfd971db2
Brian McConney <briancamcc@gmail.com>
2021-08-23 16:02:23 -0400

    "Added hashset implementation"

---
+++ JavaCourse/src/Sets/HashSetImplementation.java
@@ -0,0 +1,42 @@
+package Sets;
+
+import java.util.*;
+
+public class HashSetImplementation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * Part of the set interface
+		 * Keeps elements unique, no duplicates
+		 * Hashsets access elements in a random fashion
+		 */
+		
+		Set<String> hSet = new HashSet<String>();
+		
+		hSet.add("A");
+		hSet.add("B");
+		hSet.add("C");
+		//the second A will not be added because set doesnt
+		//tolerate duplicates
+		hSet.add("A");
+		
+		System.out.println(hSet);
+		
+		//contains returns true or false based on if the element exists
+		
+		System.out.println("List contains 'A' : " + hSet.contains("A"));
+		
+		//remove removes an item
+		
+		System.out.println(hSet);
+		
+		for(String item : hSet) {
+			System.out.println(item);
+		}
+	}
+
+}
+
+
--- JavaCourse/src/Sets/TreeSetImplementation.java
+++ JavaCourse/src/Sets/TreeSetImplementation.java
@@ -1,6 +1,6 @@
 package Sets;
 
-public class TreeSet {
+public class TreeSetImplementation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
