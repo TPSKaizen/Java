94e1c1e7f4480b4cbc850d12d22f795695167fa8
Brian McConney <briancamcc@gmail.com>
2021-08-23 17:32:11 -0400

    "Added hashMap implementation"

---
+++ JavaCourse/src/Map/hashMapImplementation.java
@@ -0,0 +1,42 @@
+package Map;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.HashMap;
+
+public class hashMapImplementation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Key = String, Value = Integer
+		Map<String, Integer> hMap = new HashMap<String, Integer>();
+		
+		hMap.put("a", 10);
+		hMap.put("b", 20);
+		hMap.put("c", 30);
+		
+		System.out.println("Size of map is: - " + hMap.size());
+		System.out.println(hMap);
+		
+		//checking if Key exists
+		if(hMap.containsKey("a")) {
+			Integer a = hMap.get("a"); //returns value that 'a' is pointing to
+			System.out.println("Value for key" + " \"a\" is : - " + a);
+		}
+		
+		//iterating over all keys via keySet
+		for(String key : hMap.keySet()) {
+			//for every key in hMap keySet print the key and the value the key is pointing to
+			System.out.println("key: " + key + ", value: " + hMap.get(key)); 
+		}
+		
+		//if we know both the key and value and we just want to iterate over it
+		for(Entry<String, Integer> entry : hMap.entrySet()) {
+			System.out.println("key " + entry.getKey() + ", value: " + entry.getValue());
+		}
+		
+		
+	}
+
+}
---
+++ JavaCourse/src/Map/mapImplementation.java
@@ -0,0 +1,24 @@
+package Map;
+
+public class mapImplementation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * Used for key value arrangements. 
+		 * Can have duplicates, but keys are unique
+		 * Uses set for store keys, uses list for values
+		 * E.g Having employee keys to access employee name
+		 * HashMap, treemap are not thread safe
+		 * Treemap sort is only applicable to the keys
+		 * HashMap access elements in random order
+		 * Hashtable is thread safe but slower
+		 * LinkedHashMap carries two properties:
+		 * 1. Property of hashmap where elements are stored/displayed in random order
+		 * 2. Linked property of LinkedList where sequence is maintained
+		 * So we can get the insertion order
+		 */
+	}
+
+}
