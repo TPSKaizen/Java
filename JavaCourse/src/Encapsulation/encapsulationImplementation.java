a7420b32e273e567e41667293218f683ff72d3bc
Brian McConney <briancamcc@gmail.com>
2021-08-25 18:50:30 -0400

    "Added encapsulation"

---
+++ JavaCourse/src/Encapsulation/encapsulationImplementation.java
@@ -0,0 +1,14 @@
+package Encapsulation;
+
+public class encapsulationImplementation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * used to protected properties of a class
+		 */
+		
+		
+	}
+
+}
